cbcff0eaa23086b9ff18c4d573c1cf9cd3c44598
IamFisherman <christianperaza2000@gmail.com>
2023-12-20 00:38:26 -0300

    Input boxes padding fixed

--- scripts/AddItem.js
+++ scripts/AddItem.js
@@ -8,16 +8,31 @@ button.addEventListener("click", function() {
     if (itemAdded.value != "") {
         // li
         const li = document.createElement("li");
+        li.classList.add("item");
 
-        // div
-        const div = document.createElement("div");
+        // div (MAIN)
+        const divMain = document.createElement("div");
+        divMain.classList.add("itemDiv");
+
+        // div (CHECK AND ITEM)
+        const divCheckItem = document.createElement("div");
+        divCheckItem.classList.add("checkItem");
         // check
         const inputCheck = document.createElement("input");
         inputCheck.setAttribute("type", "checkbox");
         inputCheck.setAttribute("class", "check");
         // p item
         const p = document.createElement("p");
+        p.classList.add("itemP");
         p.innerHTML = itemAdded.value;
+
+        // div (PRICE)
+        const divPrice = document.createElement("div");
+        divPrice.classList.add("priceDiv");
+        // p money
+        const moneyP = document.createElement("p");
+        moneyP.innerHTML = "R$";
+        moneyP.classList.add("money");
         // input price
         const inputPrice = document.createElement("input");
         inputPrice.setAttribute("type", "number");
@@ -26,6 +41,7 @@ button.addEventListener("click", function() {
 
         // delete button
         const deleteButton = document.createElement("button");
+        deleteButton.classList.add("deleteButton");
         // svg x
         const SVG_NS = "http://www.w3.org/2000/svg";
         const svg = document.createElementNS(SVG_NS, "svg");
@@ -46,16 +62,24 @@ button.addEventListener("click", function() {
         path.setAttributeNS(null, "fill", "#FF4141");
         path.setAttributeNS(null, "d", "M61.44,0c33.933,0,61.439,27.507,61.439,61.439 s-27.506,61.439-61.439,61.439C27.507,122.879,0,95.372,0,61.439S27.507,0,61.44,0L61.44,0z M73.451,39.151 c2.75-2.793,7.221-2.805,9.986-0.027c2.764,2.776,2.775,7.292,0.027,10.083L71.4,61.445l12.076,12.249 c2.729,2.77,2.689,7.257-0.08,10.022c-2.773,2.765-7.23,2.758-9.955-0.013L61.446,71.54L49.428,83.728 c-2.75,2.793-7.22,2.805-9.986,0.027c-2.763-2.776-2.776-7.293-0.027-10.084L51.48,61.434L39.403,49.185 c-2.728-2.769-2.689-7.256,0.082-10.022c2.772-2.765,7.229-2.758,9.953,0.013l11.997,12.165L73.451,39.151L73.451,39.151z");
 
+        // check and item div
+        divCheckItem.append(inputCheck);
+        divCheckItem.append(p);
+
+        // price div
+        divPrice.append(moneyP);
+        divPrice.append(inputPrice);
+
         // create svg
         g.append(path);
         svg.append(g);
         deleteButton.append(svg);
 
-        div.append(inputCheck);
-        div.append(p);
-        div.append(inputPrice);
+        // append
+        divMain.append(divCheckItem);
+        divMain.append(divPrice);
 
-        li.append(div);
+        li.append(divMain);
         li.append(deleteButton);
 
         list.append(li);
