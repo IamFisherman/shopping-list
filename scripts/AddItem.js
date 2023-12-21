9d1647d42d1fc620ec92c0041331ceff6b90b3bd
IamFisherman <christianperaza2000@gmail.com>
2023-12-20 22:20:20 -0300

    Saving logic done

--- scripts/AddItem.js
+++ scripts/AddItem.js
@@ -1,4 +1,7 @@
 const budget = document.querySelector("#budget");
+const saving = document.querySelector("#saving");
+const totalSavingDiv = document.querySelector("#totalSavingDiv");
+
 const itemAdded = document.querySelector("#itemAdded");
 const button = document.querySelector("#itemAddButton");
 const list = document.querySelector("#listItems");
@@ -62,7 +65,7 @@ button.addEventListener("click", function() {
         const path = document.createElementNS(SVG_NS, "path");
         path.setAttributeNS(null, "fill-rule", "evenodd");
         path.setAttributeNS(null, "clip-rule", "evenodd");
-        path.setAttributeNS(null, "fill", "#FF4141");
+        path.setAttributeNS(null, "fill", "#ff5757");
         path.setAttributeNS(null, "d", "M61.44,0c33.933,0,61.439,27.507,61.439,61.439 s-27.506,61.439-61.439,61.439C27.507,122.879,0,95.372,0,61.439S27.507,0,61.44,0L61.44,0z M73.451,39.151 c2.75-2.793,7.221-2.805,9.986-0.027c2.764,2.776,2.775,7.292,0.027,10.083L71.4,61.445l12.076,12.249 c2.729,2.77,2.689,7.257-0.08,10.022c-2.773,2.765-7.23,2.758-9.955-0.013L61.446,71.54L49.428,83.728 c-2.75,2.793-7.22,2.805-9.986,0.027c-2.763-2.776-2.776-7.293-0.027-10.084L51.48,61.434L39.403,49.185 c-2.728-2.769-2.689-7.256,0.082-10.022c2.772-2.765,7.229-2.758,9.953,0.013l11.997,12.165L73.451,39.151L73.451,39.151z");
 
         // check and item div
@@ -95,7 +98,6 @@ button.addEventListener("click", function() {
             list.removeChild(li);
 
             calculatePrice();
-    
         })
 
         updateTotalPrice();
@@ -129,6 +131,25 @@ function calculatePrice() {
     });
 
     totalPriceElement.innerHTML = total.toFixed(2);
+
+    // const warnedPrice = parseFloat(budget.value) - 5;
+
+    // saving
+    let moneySaved = parseFloat(budget.value) - total;
+    saving.innerHTML = moneySaved.toFixed(2);
+
+    if (total > parseFloat(budget.value)) {
+        totalSavingDiv.style.backgroundColor = "#ff5757";
+    }
+    else if (total > parseFloat(budget.value) * 0.95) {
+        totalSavingDiv.style.backgroundColor = "#f18b11";
+    }
+    else if (total < parseFloat(budget.value)) {
+        totalSavingDiv.style.backgroundColor = "#008444";
+    }
+    
+    
+    
 }
 
 updateTotalPrice();
