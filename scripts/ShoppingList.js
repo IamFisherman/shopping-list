7c17c4c6556dcd65aa52f84ef9715924c2d6d659
IamFisherman <christianperaza2000@gmail.com>
2023-12-23 15:57:40 -0300

    When checked logic improved

--- scripts/ShoppingList.js
+++ scripts/ShoppingList.js
@@ -94,18 +94,22 @@ function displayList(item)
     divMain.classList.add("itemDiv");
 
     // div (CHECK AND ITEM)
-    const divCheckItem = document.createElement("div");
-    divCheckItem.classList.add("checkItem");
+    const divCheck = document.createElement("div");
+    divCheck.classList.add("checkDiv");
     // check
     const inputCheck = document.createElement("input");
     inputCheck.setAttribute("type", "checkbox");
     inputCheck.setAttribute("class", "check");
+
+    // div (PRICE)
+    const divItemPrice = document.createElement("div");
+    divItemPrice.classList.add("itemPriceDiv");
     // p item
     const p = document.createElement("p");
     p.classList.add("itemP");
     p.innerHTML = item;
 
-    // div (PRICE)
+    // div final
     const divPrice = document.createElement("div");
     divPrice.classList.add("priceDiv");
     // p money
@@ -141,9 +145,13 @@ function displayList(item)
     path.setAttributeNS(null, "fill", "#a6a6a6");
     path.setAttributeNS(null, "d", "M61.44,0c33.933,0,61.439,27.507,61.439,61.439 s-27.506,61.439-61.439,61.439C27.507,122.879,0,95.372,0,61.439S27.507,0,61.44,0L61.44,0z M73.451,39.151 c2.75-2.793,7.221-2.805,9.986-0.027c2.764,2.776,2.775,7.292,0.027,10.083L71.4,61.445l12.076,12.249 c2.729,2.77,2.689,7.257-0.08,10.022c-2.773,2.765-7.23,2.758-9.955-0.013L61.446,71.54L49.428,83.728 c-2.75,2.793-7.22,2.805-9.986,0.027c-2.763-2.776-2.776-7.293-0.027-10.084L51.48,61.434L39.403,49.185 c-2.728-2.769-2.689-7.256,0.082-10.022c2.772-2.765,7.229-2.758,9.953,0.013l11.997,12.165L73.451,39.151L73.451,39.151z");
 
-    // check and item div
-    divCheckItem.append(inputCheck);
-    divCheckItem.append(p);
+    // check div
+    divCheck.append(inputCheck);
+    divCheck.append(divItemPrice);
+
+    // item and price div
+    divItemPrice.append(p);
+    divItemPrice.append(divPrice);
 
     // price div
     divPrice.append(moneyP);
@@ -155,8 +163,7 @@ function displayList(item)
     deleteButton.append(svg);
 
     // append
-    divMain.append(divCheckItem);
-    divMain.append(divPrice);
+    divMain.append(divCheck);
 
     li.append(divMain);
     li.append(deleteButton);
